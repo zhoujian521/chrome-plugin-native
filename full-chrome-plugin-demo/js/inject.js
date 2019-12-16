f72f55d62edf463341f4b91bc312bb48fdb14966
zhoujian <jian.zhou@litex.io>
2019-12-16 10:45:26 +0800

    init chrome-plugin-native

--- full-chrome-plugin-demo/js/inject.js
+++ full-chrome-plugin-demo/js/inject.js
@@ -1,16 +1,14 @@
 // 通过postMessage调用content-script
-function invokeContentScript(code)
-{
-	window.postMessage({cmd: 'invoke', code: code}, '*');
+function invokeContentScript(code) {
+	window.postMessage({ cmd: 'invoke', code: code }, '*');
 }
 // 发送普通消息到content-script
-function sendMessageToContentScriptByPostMessage(data)
-{
-	window.postMessage({cmd: 'message', data: data}, '*');
+function sendMessageToContentScriptByPostMessage(data) {
+	window.postMessage({ cmd: 'message', data: data }, '*');
 }
 
 // 通过DOM事件发送消息给content-script
-(function() {
+(function () {
 	var customEvent = document.createEvent('Event');
 	customEvent.initEvent('myCustomEvent', true, true);
 	// 通过事件发送消息给content-script
